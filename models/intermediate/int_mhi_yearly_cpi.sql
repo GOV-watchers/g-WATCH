451db911672e6e6569e82542160d474d87e0c945
Jahshiek <jahshiekwade58@gmail.com>
2023-09-05 02:56:31 +0000

    int_mhi_yearly_cpi model created

---
+++ models/intermediate/int_mhi_yearly_cpi.sql
@@ -0,0 +1,39 @@
+-- -- Reference the MHI and CPI models
+-- WITH CPI AS (
+--     SELECT *
+--     FROM {{ ref('stg_yearly_cpi') }}
+-- ),
+-- MHI AS (
+--     SELECT *
+--     FROM {{ ref('stg_mhi') }}
+-- )
+
+-- Reference the MHI and CPI models
+WITH MHI AS (
+    SELECT *
+    FROM {{ ref('stg_mhi') }}
+),
+CPI AS (
+    SELECT *
+    FROM {{ ref('stg_yearly_cpi') }}
+)
+-- Get the CPI of 2020
+, CPI_2020 AS (
+    SELECT
+        MAX(CPI_AVG) AS CPI_2020
+    FROM CPI
+    WHERE CPI_BY_YEAR = '2020'
+)
+
+-- Calculate the inflation-adjusted Median Household Income
+SELECT
+    M.MHI_ID,
+    M.MHI,
+    M.MHI_BY_YEAR,
+    C.CPI_AVG,
+    SUBSTRING((M.MHI * (CPI_2020.CPI_2020 / C.CPI_AVG)),1,5) AS AdjustedMHI
+FROM MHI M
+JOIN CPI C ON M.MHI_BY_YEAR = C.CPI_BY_YEAR
+JOIN CPI_2020 ON 1=1 -- This is a placeholder join condition to ensure you use the CPI_2020 value
+WHERE M.MHI_BY_YEAR >= '1980' AND M.MHI_BY_YEAR <= '2020'
+ORDER BY M.MHI_BY_YEAR;
