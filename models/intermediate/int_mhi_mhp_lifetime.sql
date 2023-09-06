7195f3966631abc6cc7ce50ae3b24e34bfe1e460
Luis F. Cordero <lf9904@gmail.com>
2023-09-06 22:58:35 +0000

    added a comment

--- models/intermediate/int_mhi_mhp_lifetime.sql
+++ models/intermediate/int_mhi_mhp_lifetime.sql
@@ -1,3 +1,5 @@
+-- 7. Based on the Median Household Income, do people today need to work longer hours to afford a house compared to the 1980/90/2000s, and does this affect their ability to own a home within their working lifetime(43 years is working lifetime)? Based on 36% of their income
+
 WITH MHI AS(
     SELECT * FROM {{ ref('stg_mhi') }}
 ),
